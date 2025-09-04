4d5807e6abfcd51de10c17a29c3d48f827a07642
helouazizi <ouazizi2code@gmail.com>
2025-09-04 13:46:27 +0100

    add

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -9,9 +9,7 @@ pub fn celsius_to_fahrenheit(c: f64) -> f64 {
 }
 
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    let c = (f - 32.0) * 5.0 / 9.0;
-    let factor = 1_000_000_000_000_000.0; // 15 decimals
-    (c * factor).round() / factor
+   (f - 32.0) /( 9.0 / 5.0)
 }
 
 
