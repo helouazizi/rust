6c0f1635e29e6b9454610d413755adc765f4e09e
helouazizi <ouazizi2code@gmail.com>
2025-09-04 14:31:04 +0100

    remove

--- looping/target/debug/deps/looping-24c60e78ba5bb720.d
+++
@@ -1,5 +0,0 @@
-/home/helouaziz/Desktop/piscine-rust/looping/target/debug/deps/looping-24c60e78ba5bb720: src/main.rs
-
-/home/helouaziz/Desktop/piscine-rust/looping/target/debug/deps/looping-24c60e78ba5bb720.d: src/main.rs
-
-src/main.rs:
--- looping/target/debug/looping.d
+++
@@ -1,1 +0,0 @@
-/home/helouaziz/Desktop/piscine-rust/looping/target/debug/looping: /home/helouaziz/Desktop/piscine-rust/looping/src/main.rs
