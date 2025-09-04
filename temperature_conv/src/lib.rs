b482495887928cf44d00b69b940c1d5b8e608e38
helouazizi <ouazizi2code@gmail.com>
2025-09-04 13:20:07 +0100

    add temp

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -4,11 +4,14 @@ pub fn add(left: usize, right: usize) -> usize {
 
 
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f-32.0) * (5.0 /9.0)
+    let c =(f-32.0) * (5.0 /9.0);
+    (c*100.0).round() / 100.0
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    (c+32.0)*(9.0/5.0)
+    let f =   (c+32.0)*(9.0/5.0) ;
+    (f*100.0).round() / 100.0
+  
 }
 
 #[cfg(test)]
@@ -17,7 +20,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = fahrenheit_to_celsius(-459.67);
+        let result = fahrenheit_to_celsius(27.0);
         assert_eq!(result, 4.0);
     }
 }
