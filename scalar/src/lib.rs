771e5733058c838fdfd1abb120f907953805192d
helouazizi <ouazizi2code@gmail.com>
2025-09-04 12:18:30 +0100

    refactore

--- scalar/src/lib.rs
+++ scalar/src/lib.rs
@@ -10,23 +10,19 @@ pub fn diff(a: i16, b: i16) -> i16 {
 
 
 pub fn sum(a: u8, b:u8) -> u8 {
-    let res =  a- b ;
-    res
+     a+ b 
 }
 
 pub fn quo(a: f32, b: f32) -> f32 {
-let res = a/b ;
-res 
+a/b 
 }
 
 pub fn pro(a: i8, b: i8) -> i8{
-let res =  a*b ;
-res
+ a*b 
 }
 
 pub fn rem(a: f32, b: f32) -> f32{
-let res =  a%b ;
-res
+ a%b 
 }
 
 #[cfg(test)]
