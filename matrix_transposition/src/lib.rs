1b675ab1ec541f8ef21a7268bbceaba9142e7840
helouazizi <ouazizi2code@gmail.com>
2025-09-04 17:43:24 +0100

    add matrix

---
+++ matrix_transposition/src/lib.rs
@@ -0,0 +1,21 @@
+
+#[derive(Debug,PartialEq)]
+
+pub struct Matrix (pub (i32,i32),pub (i32 , i32));
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+pub fn transpose(m: Matrix) -> Matrix {
+    Matrix((m.0.0,m.1.0),(m.0.1, m.1.1))
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
