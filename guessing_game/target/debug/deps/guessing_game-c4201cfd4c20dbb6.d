a524184ad7aa37cbb46b836a24ded9685707a185
helouazizi <ouazizi2code@gmail.com>
2025-09-04 10:09:36 +0100

    setup

--- guessing_game/target/debug/deps/guessing_game-c4201cfd4c20dbb6.d
+++
@@ -1,5 +0,0 @@
-/home/helouaziz/Desktop/github/rust/guessing_game/target/debug/deps/guessing_game-c4201cfd4c20dbb6: src/main.rs
-
-/home/helouaziz/Desktop/github/rust/guessing_game/target/debug/deps/guessing_game-c4201cfd4c20dbb6.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/helouaziz/Desktop/github/rust/guessing_game/target/debug/guessing_game: /home/helouaziz/Desktop/github/rust/guessing_game/src/main.rs
