04ef914ea8a2c088d3fbe33b1b5aa793ea0398ea
helouazizi <ouazizi2code@gmail.com>
2025-09-04 13:52:58 +0100

    add

---
+++ speed_transformation/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub fn km_per_hour_to_meters_per_second(km_h: f64) -> f64 {
+    km_h / 3.6
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
