27d9bf65f7d3db717899df433ac909eb2052b4dc
helouazizi <ouazizi2code@gmail.com>
2025-09-06 14:34:45 +0100

    addd

--- arrays/src/lib.rs
+++ arrays/src/lib.rs
@@ -2,6 +2,14 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn sum(a:&[i32]) -> i32 {
+    a.iter().sum()
+}
+
+pub fn thirtytwo_tens() -> [i32; 32] {
+    [10;32]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
