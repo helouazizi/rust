052ffb82ee696f32f3ebe03ebc87200675a6dd3d
helouazizi <ouazizi2code@gmail.com>
2025-09-06 11:39:00 +0100

    add some path alculation

--- circle/src/lib.rs
+++ circle/src/lib.rs
@@ -4,19 +4,41 @@ pub fn add(left: usize, right: usize) -> usize {
 
 #[derive(Debug, Clone, Copy)]
 pub struct Circle {
-	pub center //..
-	pub radius //..
+	pub center:Point ,
+	pub radius:f64
 }
 
 impl Circle {
-    // ...
+    pub  fn new(x:f64,y:f64,r:f64) ->Self {
+        Self {
+            center : Point(x,y),
+            radius : r
+        }
+    }
+
+    pub fn area(&self) -> f64 {
+      std::f64::consts::PI * self.radius * self.radius 
+    }
+
+    pub fn diameter(&self) -> f64{
+        self.radius * 2.0
+    }
+
+    pub fn intersect(&self, other_circle : Circle) -> bool {
+        let dis = self.center.distance(other_circle.center);
+        dis <= (self.radius + other_circle.radius)
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
-pub struct Point(/* */);
+pub struct Point(pub f64 ,  pub f64);
 
 impl Point {
-    // ...
+   pub  fn distance(&self, other_point :Point) -> f64 {
+        let dx = other_point.0 - self.0 ;
+        let dy =  other_point.1 - self.1;
+        (dx*dx + dy*dy).sqrt()
+    }
 }
 
 
