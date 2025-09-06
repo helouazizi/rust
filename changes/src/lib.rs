f8234084c8028d5a2eefc450cdab824890606ce2
helouazizi <ouazizi2code@gmail.com>
2025-09-06 10:45:25 +0100

    add change s

--- changes/src/lib.rs
+++ changes/src/lib.rs
@@ -1,6 +1,29 @@
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct Light {
+	pub alias: String,
+	pub brightness: u8,
+}
+
+impl Light {
+	pub fn new(alias: &str) -> Self {
+        Self {alias: alias.to_string(),brightness : 0 }
+	}
+}
+
+pub fn change_brightness(lights: &mut [Light], alias: &str, value: u8) {
+    lights.iter_mut().for_each(|light| {
+        if light.alias == alias {
+            light.brightness = value ;
+        }
+    });
+
+    
+}
+
+
 
 #[cfg(test)]
 mod tests {
