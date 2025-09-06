4a5edcf9b0ed886a234283f308246d88413d943f
helouazizi <ouazizi2code@gmail.com>
2025-09-06 10:07:58 +0100

    sssssssssssss

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -4,10 +4,16 @@ pub fn add(left: usize, right: usize) -> usize {
 
 
 pub fn arrange_phrase(phrase: &str) -> String {
-    let buf : Vec<&str> = phrase.split(' ').collect();
-    println!("{:?}", buf);
-    "".to_string()
+    let mut  buf : Vec<&str> = phrase.split(' ').collect();
+    buf.sort_by_key(|c| find_digit(c));
 
+    let res : String = buf.join(" ");
+    res
+}
+
+fn find_digit(str : &str)  -> i32 {
+    let digit : String =  str.chars().filter(|c| c.is_ascii_digit()).collect();
+    digit.parse::<i32>().unwrap_or(0)
 }
 
 
