319ab483ad7e7a4cea29bbfd7b8e37a91ee71037
helouazizi <ouazizi2code@gmail.com>
2025-09-07 10:14:54 +0100

    add some thing to hash pa

--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -1,6 +1,28 @@
+use std::collections::HashMap;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
+
+
+pub fn word_frequency_counter<'a>(words: &[&'a str])  -> HashMap<&'a str, usize> {
+    let mut  hash: HashMap<&str,usize> = HashMap::new();
+    for word in words {
+        // println!("{}",word);
+        if hash.contains_key(word) {
+            hash.insert(word,hash.get(word).unwrap()+1);
+        }else {
+            hash.insert(word,1);
+        }
+        
+    }
+    hash
+}
+
+pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
+frequency_count.len()
+}
+
 
 #[cfg(test)]
 mod tests {
