ab088a01e11ac7f40c6920a81012bfa01086e7d6
helouazizi <ouazizi2code@gmail.com>
2025-09-08 15:09:29 +0100

    add a simple initilawation

--- add_two_number/src/main.rs
+++ add_two_number/src/main.rs
@@ -1,13 +1,16 @@
 // add_two_number/src/main.rs
 
-
+#[derive(Debug)]
 pub struct ListNode {
     val : i32 ,
-    next : ListNode,
+    next : Option<Box<ListNode>>,
 }
 
 
 
 fn main() {
     println!("Hello, world!");
+    let test =  ListNode{val:0 , next : Some(Box::new(ListNode{val:1, next:None}))} ;
+    println!("{:?}",test.next.unwrap().val);
+
 }
