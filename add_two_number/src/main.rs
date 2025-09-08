300c6ce3291d8e2a9727bf8c427cc578842f334a
helouazizi <ouazizi2code@gmail.com>
2025-09-08 17:00:23 +0100

    add a function

--- add_two_number/src/main.rs
+++ add_two_number/src/main.rs
@@ -12,7 +12,7 @@ impl ListNode {
     }
 }
 
-fn to_list(vector:Vec<i32>) -> Option<Box<ListNode>> {
+fn to_list(vector:&Vec<i32>) -> Option<Box<ListNode>> {
     let mut list = None ;
     for &num in vector.iter().rev() {
         let mut node = ListNode::new(num) ;
@@ -28,6 +28,13 @@ fn main() {
     println!("Hello, world!");
     // let test =  ListNode{val:0 , next : Some(Box::new(ListNode::new(1)))} ;
     let vector = vec![0,1,2,3,4,5,6,7,8,9];
-    println!("{:?}",to_list(vector));
+    let list1 = to_list(&vector) ;
+    let list2 = to_list(&vector);
+    println!("{:?}",list1);
 
+}
+
+fn add_two_number(l1: ListNode, l2 : ListNode) -> Option<Box<ListNode>> {
+    
+    None
 }
