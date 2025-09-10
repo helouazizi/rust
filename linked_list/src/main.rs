db74558a55c38f73d4531b620a89994ffcdb08cc
helouaziz <ouazizi2code@gmail.com>
2025-09-10 13:00:17 +0100

    add a methode with eror

--- linked_list/src/main.rs
+++ linked_list/src/main.rs
@@ -1,24 +1,24 @@
 // linked_list/src/main.rs
 
-
-
-
 fn main() {
     println!("Hello, world!");
 
     let list = ListNode::new();
-    println!("{:?}",list);
+    println!("{:?}", list);
 }
 
 #[derive(Debug)]
 struct ListNode {
-    val : i32,
-    next : Option<Box<ListNode>>
+    val: i32,
+    next: Option<Box<ListNode>>,
 }
 
-impl  ListNode {
-    fn new() ->ListNode{
+impl ListNode {
+    fn new() -> ListNode {
         ListNode { val: 0, next: None }
     }
-}
 
+    fn push_front(&self, item: i32) {
+        for val in self.next.unwrap() {}
+    }
+}
