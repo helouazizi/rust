9696fb2e63728d331d821791f3185d01ccbde7f9
helouaziz <ouazizi2code@gmail.com>
2025-09-10 12:39:34 +0100

    add a simple linked list

--- linked_list/src/main.rs
+++ linked_list/src/main.rs
@@ -1,4 +1,24 @@
 // linked_list/src/main.rs
+
+
+
+
 fn main() {
     println!("Hello, world!");
+
+    let list = ListNode::new();
+    println!("{:?}",list);
 }
+
+#[derive(Debug)]
+struct ListNode {
+    val : i32,
+    next : Option<Box<ListNode>>
+}
+
+impl  ListNode {
+    fn new() ->ListNode{
+        ListNode { val: 0, next: None }
+    }
+}
+
