2e074b5f702b96c89cec65a4ba822c85d1be63ba
helouaziz <ouazizi2code@gmail.com>
2025-09-10 15:59:27 +0100

    add panic

--- panic/src/lib.rs
+++ panic/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::fs::{ File};
+
+pub fn open_file(s: &str) -> File {
+
+    File::open(s).expect("called `Result::unwrap()` on an `Err` value")
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
--- panic/src/main.rs
+++ panic/src/main.rs
@@ -12,3 +12,21 @@ fn main() {
     // this should panic!
     open_file(filename);
 }
+
+
+
+
+
+#[test]
+#[should_panic(expected = "No such file or directory")]
+fn test_opening() {
+    open_file("file.txt");
+}
+
+#[test]
+fn test_opening_existing() {
+    let filename = "created.txt";
+    File::create(filename).unwrap();
+    open_file(filename);
+    fs::remove_file(filename).unwrap();
+}
