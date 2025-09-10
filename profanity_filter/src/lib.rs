4adbd79dd8bd5ee0ae55c6c71e9676611199a32e
helouaziz <ouazizi2code@gmail.com>
2025-09-10 16:32:41 +0100

    add

--- profanity_filter/src/lib.rs
+++ profanity_filter/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn check_ms(message: &str) -> Result<&str, &str> {
+    if message.contains("stupid") || message.is_empty() {
+         Err("ERROR: illegal")
+    }else {
+        Ok(message)
     }
+    
 }
