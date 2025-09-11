a761e0bb0dad599b656744824ddd3d252551104c
helouazizi <ouazizi2code@gmail.com>
2025-09-11 11:37:21 +0100

    add  .

--- banner/src/lib.rs
+++ banner/src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{collections::HashMap, num::ParseFloatError};
+
+pub struct Flag {
+    // expected public fields
+    short_hand: String,
+
+    long_hand: String,
+
+    desc: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<'a> Flag<'a> {
+    pub fn opt_flag(name: &'a str, d: &'a str) -> Self {
+        Self{ short_hand : "-" , long_hand : "--" , desc : d}
     }
+}
+
+pub type Callback = fn(&str, &str) -> Result<String, ParseFloatError>;
+
+pub struct FlagsHandler {
+    pub flags: HashMap<String, Callback>,
+}
+
+impl FlagsHandler {
+    pub fn add_flag(&mut self, flag: Flag, func: Callback) {
+        todo!()
+    }
+
+    pub fn exec_func(&self, input: &str, argv: &[&str]) -> Result<String, String> {
+        todo!()
+    }
+}
+
+pub fn div(a: &str, b: &str) -> Result<String, ParseFloatError> {
+    todo!()
+}
+
+pub fn rem(a: &str, b: &str) -> Result<String, ParseFloatError> {
+    todo!()
 }
