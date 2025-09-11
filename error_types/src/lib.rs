6e2987a768628a31900b33f4f07da671a20b88ab
helouaziz <ouazizi2code@gmail.com>
2025-09-11 17:30:03 +0100

    addd

--- error_types/src/lib.rs
+++ error_types/src/lib.rs
@@ -3,10 +3,9 @@ use chrono::NaiveDateTime;
 // this will be the structure that wil handle the errors
 #[derive(Debug, Eq, PartialEq)]
 pub struct FormError {
-    // expected public fields
-    pub form_values : (String,String),
-    pub date : String ,
-    pub err : String,
+    pub form_values: (&'static str, String),
+    pub date: String,
+    pub err: &'static str,
 }
 #[derive(Debug, Eq, PartialEq)]
 pub struct Form {
@@ -14,9 +13,9 @@ pub struct Form {
     pub password: String,
 }
 
-impl FormError {
+impl  FormError  {
     pub fn new(field_name: &'static str, field_value: String, err: &'static str) -> Self {
-        Self { form_values: (field_name.to_string() , field_value), date: chrono::Local::now().naive_local().format("%Y-%m-%d %H:%M:%S").to_string()  , err: err.to_string() }
+        Self { form_values: (field_name , field_value.to_string()), date: chrono::Local::now().naive_local().format("%Y-%m-%d %H:%M:%S").to_string()  , err: err }
     }
 }
 
@@ -25,14 +24,14 @@ impl FormError {
 
 impl Form {
     pub fn validate(&self) -> Result<(), FormError> {
-        let date  =  chrono::Local::now().naive_local().format("%Y-%m-%d %H:%M:%S").to_string() ;
+        let date :String  =  chrono::Local::now().naive_local().format("%Y-%m-%d %H:%M:%S").to_string() ;
         // todo!()
         if self.name.len() < 1 {
-           return   Err(FormError{err:"Username is empty".to_string() , form_values: ("Username".to_string(),self.name.to_string()) , date :date} );
+           return   Err(FormError{err:"Username is empty" , form_values: ("name",self.name.to_string()) , date :date} );
         }
 
         if self.password.len() < 8 {
-             return  Err(FormError{err:"Password should be at least 8 characters long".to_string() , form_values: ("Password".to_string(),self.password.to_string()) , date : date} ) ;
+             return  Err(FormError{err:"Password should be at least 8 characters long" , form_values: ("password",self.password.to_string()) , date : date} ) ;
         }
 
         let isdigit =  self.password.chars().any(|c| c.is_ascii_digit());
@@ -40,7 +39,7 @@ impl Form {
         let ispenc =  self.password.chars().any(|c| (c as u8 >= 0x24 && c as u8 <= 0x2F) || c.is_ascii_punctuation()  );
 
         if !(isdigit && isletter && ispenc ){
-             return  Err(FormError{err:"Password should be a combination of ASCII numbers, letters and symbols".to_string() , form_values: ("Password".to_string(),self.password.to_string()) , date :date} ) ;
+             return  Err(FormError{err:"Password should be a combination of ASCII numbers, letters and symbols" , form_values: ("password", self.password.to_string()) , date :date} ) ;
         }
 
          Ok(())
