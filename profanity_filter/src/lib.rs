431ec668247d3d3d44e40f5cf7711fbbbdcc52cb
Christopher  Fremond <dev@01-edu.org>
2025-09-11 09:41:22 +0000

    Update profanity_filter/src/lib.rs

--- profanity_filter/src/lib.rs
+++ profanity_filter/src/lib.rs
@@ -1,8 +1,10 @@
+// Empty file
+
 pub fn check_ms(message: &str) -> Result<&str, &str> {
-    if message.contains("stupid") || message.is_empty() {
-         Err("ERROR: illegal")
-    }else {
-        Ok(message)
-    }
+  if message.contains("stupid") || message.is_empty() {
+    Ok("hello there")
     
+  }else{
+    Err("ERROR: illegal")
+  }
 }
