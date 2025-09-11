fbdd99cf0b3a8ecddb9cd03c5bc8769e99182987
helouaziz <ouazizi2code@gmail.com>
2025-09-11 14:41:29 +0100

    add

--- banner/src/lib.rs
+++ banner/src/lib.rs
@@ -1,20 +1,14 @@
-//
+ use std::{collections::HashMap, num::ParseFloatError};
 
-
-use std::{collections::HashMap, num::ParseFloatError};
-// lqst co,,it 
 pub struct Flag {
-    // expected public fields
-    short_hand: String,
-
+    short_hand: String, 
     long_hand: String,
-
-    desc: String,
+    desc: String
 }
 
 impl Flag {
-    pub fn opt_flag(name: &str, d:  &str) -> Self {
-        Self{ short_hand : format!("-{}",name.chars().next().unwrap_or_default()) , long_hand :  format!("-{}",name) , desc : d.to_string()}
+    pub fn opt_flag(name: &str, d: &str) -> Self {
+       Self { short_hand: format!("-{}",name.chars().nth(0).unwrap()), long_hand: format!("--{name}"), desc: format!("{d}") }
     }
 }
 
@@ -26,39 +20,41 @@ pub struct FlagsHandler {
 
 impl FlagsHandler {
     pub fn add_flag(&mut self, flag: Flag, func: Callback) {
-        self.flags.insert(flag.short_hand , func);
-        self.flags.insert(flag.long_hand , func);
-         self.flags.insert(flag.desc , func);
+        self.flags.insert(flag.short_hand, func);
+        self.flags.insert(flag.long_hand, func);
+        
     }
 
     pub fn exec_func(&self, input: &str, argv: &[&str]) -> Result<String, String> {
-
-        if let Some(callback) =  self.flags.get(input) {
-            match  callback(argv[0],argv[1]) {
+         if let  Some(callback) = self.flags.get(input){
+            match callback(argv[0], argv[1]) {
                 Ok(res) => Ok(res),
-                Err(e) => Err(format!("{}",e.to_string()))
+                Err(e) => Err(format!("{}", e.to_string())),
+            } 
+        } else {
+                Err("invalid float literal".to_string())
+
             }
-        }else {
-            Err("invalid float literal".to_string())
-        }
+
+         }
+       
+           
+        
+         
     }
-}
+
 
 pub fn div(a: &str, b: &str) -> Result<String, ParseFloatError> {
-    let a64 : f64 = a.parse()?;
-    let b64 : f64 = b.parse()?;
-    let dev : f64 =  a64 /b64 ;
-    Ok(dev.to_string())
-}
+    let a_parse: f64 = a.parse()?;
+    let b_parse: f64 = b.parse()?;
+    let c = a_parse/b_parse ;
+    Ok(c.to_string())
 
+}
 
 pub fn rem(a: &str, b: &str) -> Result<String, ParseFloatError> {
-    let a64 : f64 = a.parse()?;
-    let b64 : f64 = b.parse()?;
-    let dev : f64 =  a64 % b64 ;
-    Ok(dev.to_string())
+    let a_parse: f64 = a.parse()?;
+    let b_parse: f64 = b.parse()?;
+    let c = a_parse%b_parse ;
+    Ok(c.to_string())
 }
-
-
-
- 
