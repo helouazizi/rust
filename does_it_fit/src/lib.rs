7f82dd382ba64584530dbf9d7f3215e06395d970
helouaziz <ouazizi2code@gmail.com>
2025-09-12 11:55:13 +0100

    add

--- does_it_fit/src/lib.rs
+++ does_it_fit/src/lib.rs
@@ -1,14 +1,67 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod areas_volumes;
+
+pub use areas_volumes::{GeometricalShapes, GeometricalVolumes};
+
+
+pub fn area_fit(
+    (x, y): (usize, usize),
+    kind: areas_volumes::GeometricalShapes,
+    times: usize,
+    (a, b): (usize, usize),
+) -> bool {
+
+    // i need to extract the he ares depend on the kind passed as param all area function exist in area volumes and 
+    let area = match kind {
+        areas_volumes   ::GeometricalShapes::Square =>{
+           areas_volumes::square_area(a)
+        }
+        areas_volumes   ::GeometricalShapes::Rectangle =>{
+             areas_volumes::rectangle_area(a,b)
+        }
+        areas_volumes   ::GeometricalShapes::Circle =>{
+             areas_volumes::circle_area(a) as usize
+        }
+        areas_volumes   ::GeometricalShapes::Triangle =>{
+           areas_volumes::triangle_area(a,b) as usize
+        }
+
+
+    };
+   
+
+    area * times <=  x*y
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn volume_fit(
+    (x, y, z): (usize, usize, usize),
+    kind: areas_volumes::GeometricalVolumes,
+    times: usize,
+    (a, b, c): (usize, usize, usize),
+) -> bool {
+        let vol = match kind {
+        areas_volumes   ::GeometricalVolumes::Cone =>{
+           areas_volumes::cone_volume(a , b) as usize
+        }
+        areas_volumes   ::GeometricalVolumes::Parallelepiped =>{
+             areas_volumes::parallelepiped_volume(a,b,c)
+        }
+        areas_volumes   ::GeometricalVolumes::Sphere =>{
+             areas_volumes::sphere_volume(a) as usize
+        }
+        areas_volumes   ::GeometricalVolumes::Cube =>{
+           areas_volumes::cube_volume(a) as usize
+        }
+         areas_volumes   ::GeometricalVolumes::TriangularPyramid =>{
+           areas_volumes::triangular_pyramid_volume(a as f64,b) as usize
+        }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+
+    };
+
+
+    vol * times <=   areas_volumes::parallelepiped_volume(x,y,z)
 }
+
+
+
+ 
--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,15 +1,15 @@
-use chrono::{NaiveDate,Datelike, Weekday};
+use chrono::*;
 
 pub fn middle_day(year: u32) -> Option<Weekday> {
-    let year_i32 = year as i32;
+    // let year_i32 = year;
 
-    if is_leap_year(year_i32) {
+    if is_leap_year(year  as i32) {
         return None;
     }
 
     let middle_day = (365 + 1) / 2;
 
-    NaiveDate::from_yo_opt(year_i32, middle_day).map(|date| date.weekday())
+    NaiveDate::from_yo_opt(year as i32, middle_day).map(|date| date.weekday())
 }
 
 fn is_leap_year(year: i32) -> bool {
