64d039d94ee2bde91ffdac88687640d1c5bf6969
helouaziz <ouazizi2code@gmail.com>
2025-09-13 13:10:43 +0100

    add

--- rgb_match/src/lib.rs
+++ rgb_match/src/lib.rs
@@ -12,11 +12,9 @@ impl Color {
 
 
     pub fn swap(mut self, first: u8, second: u8) -> Color {
-        // Detect which fields correspond to the values 'first' and 'second'
         let mut first_field = None;
         let mut second_field = None;
 
-        // Check each field for 'first'
         if self.r == first {
             first_field = Some("r");
         } else if self.g == first {
@@ -27,7 +25,6 @@ impl Color {
             first_field = Some("a");
         }
 
-        // Check each field for 'second'
         if self.r == second {
             second_field = Some("r");
         } else if self.g == second {
@@ -38,12 +35,10 @@ impl Color {
             second_field = Some("a");
         }
 
-        // If either field is None, just return self unchanged
         if first_field.is_none() || second_field.is_none() {
             return self;
         }
 
-        // Swap the values of the detected fields
         match (first_field.unwrap(), second_field.unwrap()) {
             ("r", "g") | ("g", "r") => std::mem::swap(&mut self.r, &mut self.g),
             ("r", "b") | ("b", "r") => std::mem::swap(&mut self.r, &mut self.b),
@@ -51,7 +46,6 @@ impl Color {
             ("g", "b") | ("b", "g") => std::mem::swap(&mut self.g, &mut self.b),
             ("g", "a") | ("a", "g") => std::mem::swap(&mut self.g, &mut self.a),
             ("b", "a") | ("a", "b") => std::mem::swap(&mut self.b, &mut self.a),
-            // If the fields are the same, no swap needed
             _ => {}
         }
 
