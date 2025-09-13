9f9537995ca84d7af0dfda936f69c299ed22f37e
helouaziz <ouazizi2code@gmail.com>
2025-09-13 10:47:33 +0100

    adddd

--- mobs/src/lib.rs
+++ mobs/src/lib.rs
@@ -1,3 +1,9 @@
 pub mod mobs;
-use mobs::*;
+// use mobs::*;
 
+ use crate::mobs::Mob;
+ use crate::mobs::Boss;
+ use crate::mobs::Member;
+ use crate::mobs::Role;
+ 
+//  use  crate::mobs::mobs::Mob;
