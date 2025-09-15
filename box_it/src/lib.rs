1ff76af4c4d5bab39f6c6ac9149a5e015789931f
helouaziz <ouazizi2code@gmail.com>
2025-09-15 14:37:24 +0100

    add lib

--- box_it/src/lib.rs
+++ box_it/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn parse_into_boxed(s: String) -> Vec<Box<u32>> {
+    todo!()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn into_unboxed(a: Vec<Box<u32>>) -> Vec<u32> {
+    todo!()
 }
