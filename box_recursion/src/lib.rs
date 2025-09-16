e13be2207d256892757850bc964036a11bcfc2b4
helouaziz <ouazizi2code@gmail.com>
2025-09-16 18:26:58 +0100

    add box_recursion

--- box_recursion/src/lib.rs
+++ box_recursion/src/lib.rs
@@ -1,18 +1,21 @@
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Role {
     CEO,
     Manager,
     Worker,
 }
 
-impl From<&str> for Role {}
-
-#[derive(Debug)]
-pub struct WorkEnvironment {
-    pub grade: Link,
+impl From<&str> for Role {
+    fn from(s: &str) -> Self {
+        match s {
+            "CEO" => Role::CEO,
+            "Manager" => Role::Manager,
+            _ => Role::Worker, // anything else defaults to Worker
+        }
+    }
 }
 
-pub type Link = _; // Complete type alias
+pub type Link = Option<Box<Worker>>;
 
 #[derive(Debug)]
 pub struct Worker {
@@ -21,20 +24,38 @@ pub struct Worker {
     pub next: Link,
 }
 
+#[derive(Debug)]
+pub struct WorkEnvironment {
+    pub grade: Link,
+}
+
 impl WorkEnvironment {
     pub fn new() -> Self {
-        todo!()
+        Self { grade: None }
     }
 
+    // Add worker at the start of the list
     pub fn add_worker(&mut self, name: &str, role: &str) {
-        todo!()
+        let new_worker = Box::new(Worker {
+            role: Role::from(role),
+            name: name.to_string(),
+            next: self.grade.take(),
+        });
+        self.grade = Some(new_worker);
     }
 
+    // Remove the first worker (last added)
     pub fn remove_worker(&mut self) -> Option<String> {
-        todo!()
+        if let Some(mut head) = self.grade.take() {
+            self.grade = head.next.take(); // move head to next
+            Some(head.name)
+        } else {
+            None
+        }
     }
 
+    // Return the last added worker (head of the list)
     pub fn last_worker(&self) -> Option<(String, Role)> {
-        todo!()
+        self.grade.as_ref().map(|w| (w.name.clone(), w.role.clone()))
     }
 }
