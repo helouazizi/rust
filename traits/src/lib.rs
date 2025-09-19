056d9402fcaf1a00b968bb4d497bfeb9cb9c3326
helouaziz <ouazizi2code@gmail.com>
2025-09-19 13:19:02 +0100

    add final teraits

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -1,33 +1,60 @@
+
+
+use std::fmt;
+
+impl fmt::Display for Player {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}\nStrength: {}, Score: {}, Money: {}\nWeapons: {:?}",
+            self.name, self.strength as i64, self.score, self.money, self.weapons
+        )
+    }
+}
+
+
+
+
+
 #[derive(Debug)]
 pub struct Player {
-	pub name: String,
-	pub strength: f64,
-	pub score: i32,
-	pub money: i32,
-	pub weapons: Vec<String>,
+    pub name: String,
+    pub strength: f64,
+    pub score: i32,
+    pub money: i32,
+    pub weapons: Vec<String>,
 }
 
 pub struct Fruit {
-	pub weight_in_kg: f64,
+    pub weight_in_kg: f64,
 }
 
 pub struct Meat {
-	pub weight_in_kg: f64,
-	pub fat_content: f64,
+    pub weight_in_kg: f64,
+    pub fat_content: f64,
 }
 
 impl Player {
-	fn eat(&mut self, food: T) {
-		self.strength += food.gives();
-	}
+    pub fn eat<T: Food>(&mut self, food: T) {
+        self.strength += food.gives();
+    }
 }
 
 pub trait Food {
-	fn gives(&self) -> f64;
+    fn gives(&self) -> f64;
 }
 
 impl Food for Fruit {
+    fn gives(&self) -> f64 {
+        self.weight_in_kg * 4.0
+    }
 }
 
 impl Food for Meat {
+    fn gives(&self) -> f64 {
+        let protein_weight = self.weight_in_kg * (1.0 - self.fat_content);
+        let fat_weight = self.weight_in_kg * self.fat_content;
+
+        protein_weight * 4.0 + fat_weight * 9.0
+    }
 }
--- traits/src/main.rs
+++ traits/src/main.rs
@@ -16,11 +16,74 @@ fn main() {
 		strength: 1.0,
 		score: 0,
 		money: 0,
-		weapons: vec![String::from("knife")],
+		weapons: vec![String::from("knife"), String::from("shotgun")]
 	};
 	println!("Before eating {:?}", player1);
 	player1.eat(apple);
 	println!("After eating an apple\n{}", player1);
 	player1.eat(steak);
 	println!("After eating a steak\n{}", player1);
+}
+
+
+
+#[test]
+fn test_gives() {
+    let apple = Fruit { weight_in_kg: 1.0 };
+    assert_eq!(apple.gives(), 4.0);
+    let steak = Meat {
+        weight_in_kg: 1.0,
+        fat_content: 1.0,
+    };
+    assert_eq!(steak.gives(), 9.0);
+
+    let steak = Meat {
+        weight_in_kg: 1.0,
+        fat_content: 0.0,
+    };
+    assert_eq!(steak.gives(), 4.0);
+
+    let steak = Meat {
+        weight_in_kg: 1.5,
+        fat_content: 0.3,
+    };
+    assert_eq!(steak.gives(), 8.25);
+}
+
+#[test]
+fn test_eat() {
+    let apple = Fruit { weight_in_kg: 1.0 };
+    assert_eq!(apple.gives(), 4.0);
+    let steak = Meat {
+        weight_in_kg: 1.0,
+        fat_content: 1.0,
+    };
+
+    let mut player1 = Player {
+        name: String::from("player1"),
+        strength: 1.0,
+        score: 0,
+        money: 0,
+        weapons: vec![String::from("knife")],
+    };
+    player1.eat(apple);
+    assert_eq!(player1.strength, 5.0);
+    player1.eat(steak);
+    assert_eq!(player1.strength, 14.0);
+}
+
+#[test]
+fn test_display() {
+    let player1 = Player {
+        name: String::from("player1"),
+        strength: 1.0,
+        score: 0,
+        money: 0,
+        weapons: vec![String::from("knife"), String::from("shotgun")],
+    };
+    println!("{}", player1);
+    assert_eq!(
+        player1.to_string(),
+        "player1\nStrength: 1, Score: 0, Money: 0\nWeapons: [\"knife\", \"shotgun\"]"
+    )
 }
