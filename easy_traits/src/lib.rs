cae904856f579a575f9f70f16847e0eb66d88758
helouaziz <ouazizi2code@gmail.com>
2025-09-19 12:43:40 +0100

    final easy traits

--- easy_traits/src/lib.rs
+++ easy_traits/src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Clone)]
+pub struct StringValue {
+    pub value: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub trait AppendStr {
+    fn append_str(&mut self, str_to_append: String) -> Self;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn append_number(&mut self, nb_to_append: f64) -> Self;
+
+    fn remove_punctuation_marks(&mut self) -> Self;
+}
+
+impl AppendStr for StringValue {
+     fn append_str(&mut self, str_to_append: String) -> Self {
+        self.value.push_str(&str_to_append);
+        Self { value: self.value.clone() }
+     }
+      fn append_number(&mut self, nb_to_append: f64) -> Self {
+         let string = nb_to_append.to_string();
+        self.value.push_str(&string);
+        Self { value: self.value.clone() }
+      }
+
+
+      fn remove_punctuation_marks(&mut self) -> Self{
+          println!("{}",self.value);
+        let clean :String  = self.value.chars().filter(|c| *c == '-' || !c.is_ascii_punctuation()).collect();
+        println!("{}",clean);
+        self.value = clean ;
+        Self { value:self.value.clone() }
+      }
+
+   
 }
