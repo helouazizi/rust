8874a0342c09e418db0bfd7a77945f03fb1277d6
helouaziz <ouazizi2code@gmail.com>
2025-09-19 12:45:41 +0100

    add lib code

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -1,14 +1,33 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug)]
+pub struct Player {
+	pub name: String,
+	pub strength: f64,
+	pub score: i32,
+	pub money: i32,
+	pub weapons: Vec<String>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct Fruit {
+	pub weight_in_kg: f64,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub struct Meat {
+	pub weight_in_kg: f64,
+	pub fat_content: f64,
+}
+
+impl Player {
+	fn eat(&mut self, food: T) {
+		self.strength += food.gives();
+	}
+}
+
+pub trait Food {
+	fn gives(&self) -> f64;
+}
+
+impl Food for Fruit {
+}
+
+impl Food for Meat {
 }
