17240e26d9a95eedc837bd618cd47356e82bca48
helouazizi <ouazizi2code@gmail.com>
2025-09-20 17:26:03 +0100

    add

--- lalgebra_vector/src/lib.rs
+++ lalgebra_vector/src/lib.rs
@@ -1,6 +1,11 @@
 use std::ops::{Add, Div, Mul, Sub};
 
-pub trait Scalar : Copy + Add<Output = Self> + Sub<Output = Self> + Mul<Output = Self> + Div<Output = Self> {
+pub trait Scalar: Copy
+    + Add<Output = Self>
+    + Sub<Output = Self>
+    + Mul<Output = Self>
+    + Div<Output = Self>
+{
     fn zero() -> Self;
     fn one() -> Self;
 }
@@ -9,27 +14,22 @@ impl Scalar for i32 {
     fn zero() -> Self { 0 }
     fn one() -> Self { 1 }
 }
-
 impl Scalar for f64 {
     fn zero() -> Self { 0.0 }
     fn one() -> Self { 1.0 }
 }
-
 impl Scalar for u32 {
     fn zero() -> Self { 0 }
     fn one() -> Self { 1 }
 }
-
 impl Scalar for u64 {
     fn zero() -> Self { 0 }
     fn one() -> Self { 1 }
 }
-
 impl Scalar for i64 {
     fn zero() -> Self { 0 }
     fn one() -> Self { 1 }
 }
-
 impl Scalar for f32 {
     fn zero() -> Self { 0.0 }
     fn one() -> Self { 1.0 }
@@ -43,31 +43,30 @@ impl<T: Scalar> Vector<T> {
         Self(data)
     }
 
-  pub fn dot(&self, other: &Self) -> T {
-    if self.0.len() != other.0.len() {
-        panic!("Vector size mismatch in dot product");
-    }
+    pub fn dot(&self, other: &Self) -> Option<T> {
+        if self.0.len() != other.0.len() {
+            return None;
+        }
 
-    let mut res = T::zero();
-    for i in 0..self.0.len() {
-        res = res + self.0[i] * other.0[i];
+        let mut res = T::zero();
+        for i in 0..self.0.len() {
+            res = res + self.0[i] * other.0[i];
+        }
+        Some(res)
     }
-    res
-}
-
 }
 
 impl<T: Scalar> Add for Vector<T> {
-    type Output = Self;
+    type Output = Option<Self>;
 
     fn add(self, rhs: Self) -> Self::Output {
         if self.0.len() != rhs.0.len() {
-            panic!("Vector size mismatch");
+            return None;
         }
         let data = self.0.into_iter()
             .zip(rhs.0.into_iter())
             .map(|(a, b)| a + b)
             .collect();
-        Vector(data)
+        Some(Vector(data))
     }
 }
