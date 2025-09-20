01a9a7303456aee61e3b1df7e86a0e4f9759df25
helouazizi <ouazizi2code@gmail.com>
2025-09-20 11:46:06 +0100

    add lib

--- vector_operations/src/lib.rs
+++ vector_operations/src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct ThreeDVector<T> {
+	pub i: T,
+	pub j: T,
+	pub k: T,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use std::ops::{Add, Sub};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+impl Add for ThreeDVector<T> {
+}
+
+impl Sub for ThreeDVector<T> {
 }
