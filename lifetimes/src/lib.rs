bb383cf17df113181d4f21b36d283fa302461b2d
helouaziz <ouazizi2code@gmail.com>
2025-09-24 15:26:29 +0100

    add lib content

--- lifetimes/src/lib.rs
+++ lifetimes/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug)]
+pub struct Person{
+	pub name: &str,
+	pub age: u8,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+impl Person {
+	pub fn new(name: &str) -> Person {
+	}
 }
