bc02e70270fc84b2596661c97381abbe5955c157
helouaziz <ouazizi2code@gmail.com>
2025-09-24 16:20:13 +0100

    add final croos code

--- border_cross/src/lib.rs
+++ border_cross/src/lib.rs
@@ -1,30 +1,43 @@
-
 pub struct Car<'a> {
-	pub plate_nbr: &'a str,
-	pub model: &'a str,
-	pub horse_power: u32,
-	pub year: u32,
+    pub plate_nbr: &'a str,
+    pub model: &'a str,
+    pub horse_power: u32,
+    pub year: u32,
 }
 
-
 pub struct Truck<'a> {
-	pub plate_nbr: &'a str,
-	pub model: &'a str,
-	pub horse_power: u32,
-	pub year: u32,
-	pub load_tons: u32,
+    pub plate_nbr: &'a str,
+    pub model: &'a str,
+    pub horse_power: u32,
+    pub year: u32,
+    pub load_tons: u32,
 }
 
 pub trait Vehicle {
-	fn model(&self) -> &str;
-	fn year(&self) -> u32;
+    fn model(&self) -> &str;
+    fn year(&self) -> u32;
 }
 
 impl Vehicle for Truck<'_> {
+    fn model(&self) -> &str {
+        self.model
+    }
+    fn year(&self) -> u32 {
+        self.year
+    }
 }
 
 impl Vehicle for Car<'_> {
+    fn model(&self) -> &str {
+        self.model
+    }
+    fn year(&self) -> u32 {
+        self.year
+    }
 }
 
-fn all_models(list: Vec<&Vehicle>) -> Vec<&str> {
+pub fn all_models(list: Vec<&dyn Vehicle>) -> Vec<&str> {
+
+	let res : Vec<&str> = list.iter().map(|v| v.model()).collect();
+	res
 }
