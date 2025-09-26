51400b896ed1d6fdc6e54a467c2b07b0e2a64171
helouaziz <ouazizi2code@gmail.com>
2025-09-26 11:31:44 +0100

    final

--- sales/src/lib.rs
+++ sales/src/lib.rs
@@ -2,6 +2,7 @@
 pub struct Store {
     pub products: Vec<(String, f32)>,
 }
+
 impl Store {
     pub fn new(products: Vec<(String, f32)>) -> Store {
         Store { products }
@@ -10,10 +11,47 @@ impl Store {
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Cart {
-    // expected public fields
+    pub items: Vec<(String, f32)>,
+    pub receipt: Vec<f32>,
 }
+
 impl Cart {
-    pub fn new() -> Cart {}
-    pub fn insert_item(&mut self, s: &Store, ele: String) {}
-    pub fn generate_receipt(&mut self) -> Vec<f32> {}
+    pub fn new() -> Cart {
+        Cart {
+            items: Vec::new(),
+            receipt: Vec::new(),
+        }
+    }
+
+    pub fn insert_item(&mut self, s: &Store, ele: String) {
+        if let Some((name, price)) = s.products.iter().find(|(n, _)| *n == ele) {
+            self.items.push((name.clone(), *price));
+        }
+    }
+
+    pub fn get_prices(&self) -> Vec<f32> {
+        self.items.iter().map(|(_, v)| *v).collect::<Vec<f32>>()
+    }
+
+    pub fn generate_receipt(&mut self) -> Vec<f32> {
+        let mut prices = self.get_prices();
+        let cal = self.items.len() / 3;
+        prices.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+        let v: Vec<f32> = prices[cal..].to_vec();
+
+        let percentage: f32 =
+            (v.iter().sum::<f32>() * 100.0) as f32 / prices.iter().sum::<f32>() as f32;
+
+        self.receipt = prices
+            .iter()
+            .map(|price| round_two(price * percentage / 100.0))
+            .collect::<Vec<f32>>();
+
+        self.receipt.clone()
+    }
+}
+
+fn round_two(nbr: f32) -> f32 {
+    (nbr * 100.0).round() / 100.0
 }
