8d883f08ebb79c3e854fc90109dae39383c11f4f
helouaziz <ouazizi2code@gmail.com>
2025-09-26 17:48:44 +0100

    add get products

--- get_products/src/lib.rs
+++ get_products/src/lib.rs
@@ -1,3 +1,8 @@
 pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
-
+        if arr.len() == 1 {
+            return  Vec::new();
+        }
+        arr.iter()
+        .map(|&num| arr.iter().filter(|&&x| x != num).product())
+        .collect()
 }
