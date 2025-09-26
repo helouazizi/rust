05bfad2c6be84b03024ea22734031bdc9c674394
helouaziz <ouazizi2code@gmail.com>
2025-09-26 11:08:38 +0100

    add sales lib

--- sales/src/lib.rs
+++ sales/src/lib.rs
@@ -1,14 +1,19 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug, Clone, PartialEq)]
+pub struct Store {
+    pub products: Vec<(String, f32)>,
+}
+impl Store {
+    pub fn new(products: Vec<(String, f32)>) -> Store {
+        Store { products }
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(Debug, Clone, PartialEq)]
+pub struct Cart {
+    // expected public fields
+}
+impl Cart {
+    pub fn new() -> Cart {}
+    pub fn insert_item(&mut self, s: &Store, ele: String) {}
+    pub fn generate_receipt(&mut self) -> Vec<f32> {}
 }
